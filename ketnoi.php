09dba35aee50a4807a59f65ab317c8770e06fa80
Thuanle <thuanhinh2@gmail.com>
2022-11-28 17:10:06 +0700

    none

--- ketnoi.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$dbHost = 'localhost';
-	$dbUser = 'root';
-	$dbPass = '';
-	$dbName = 'beephoneshop';
-	$dbConnect = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
-?>
--- xoasp.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$id_sp = $_GET['id_sp'];
-		include_once('ketnoi.php');
-		$sql = "DELETE FROM sanpham WHERE id_sp = $id_sp ";
-		$query = mysqli_query($dbConnect, $sql);
-		header('location: danhsachsp.php?page_layout=danhsachsp');
-
-?>
