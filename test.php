bb1070296a02caa79fb3bfd0b3cf2f927db0f561
Dath <datvu9620@gmail.com>
2022-12-04 17:43:52 +0700

    none

--- test.php
+++ test.php
@@ -1,7 +1,3 @@
 <?php
-	$id_sp = $_GET['id_sp'];
-    echo $id_sp;
-    echo $_SESSION['cart']['$id_sp'];
 		unset($_SESSION['cart'][$_GET['id_sp']]);
-		// header('location: trangchu.php?page_layout=giohang');
 ?>
--- xoagiohang.php
+++ xoagiohang.php
@@ -1,7 +1,4 @@
 <?php
-	$id_sp = $_GET['id_sp'];
-    echo 'id_sp';
-    $id_sp => String;
-		unset($_SESSION['cart']['toString(id_sp)']);
-		// header('location: trangchu.php?page_layout=giohang');
+		unset($_SESSION['cart'][$_GET['id_sp']]);
+        header('location: trangchu.php?page_layout=giohang');
 ?>
